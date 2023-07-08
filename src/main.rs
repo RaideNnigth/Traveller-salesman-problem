6ebf806239332702858ce6e2164d8118432a8b0d
Gustavo Pinzon Pereira <32282947+RaideNnigth@users.noreply.github.com>
2023-07-08 17:40:37 -0300

    Getting the string to an Vec of ints
    getting all numbers in the string to an vec of i32 and them counting the number of columns on it

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::process;
 use std::fs::File;
 use std::io::Read;
+use regex::Regex;
 
 fn main() {
     // Getting the arguments from the command line
@@ -27,10 +28,25 @@ fn main() {
             process::exit(1);
         }
     };
-        
+
+    // Read the file into a string        
     let mut content = String::new();
     file.read_to_string(&mut content)
         .expect("Should have been able to read the file");
-    println!("{}", content);
+    
+    // Create a regex to find all the numbers in the file
+    let regex = Regex::new(r"\d+").unwrap();
+
+    // Find all the numbers in the file and put them in a vector
+    let numbers: Vec<i32> = regex
+        .find_iter(content.as_str())
+        .map(|m| m.as_str().parse::<i32>().unwrap())
+        .collect();
+
+    // Print the numbers
+    let num_of_columns = (numbers.len() as f64).sqrt() as u32;
+    println!("Number of columns: {}", num_of_columns);
+    println!("{:?}", numbers);
+    
     
 }
