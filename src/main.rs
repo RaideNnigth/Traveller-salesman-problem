d290a4a0f86850ef40e600df2208da9b4c900806
Gustavo Pinzon Pereira <32282947+RaideNnigth@users.noreply.github.com>
2023-07-08 20:31:31 -0300

    Update main.rs
    Creating a matrix of all vertixes in the adjacent matrix from file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::fs::File;
 use std::io::Read;
 use regex::Regex;
 
+
+
 fn main() {
     // Getting the arguments from the command line
     // The first argument is the name of the program
@@ -32,21 +34,21 @@ fn main() {
     // Read the file into a string        
     let mut content = String::new();
     file.read_to_string(&mut content)
-        .expect("Should have been able to read the file");
+        .expect("Should have been able to read the file");    
     
+    // Create a vector of vectors to store the graph
+    let mut graph: Vec<Vec<i32>> = Vec::new();
     // Create a regex to find all the numbers in the file
     let regex = Regex::new(r"\d+").unwrap();
-
-    // Find all the numbers in the file and put them in a vector
-    let numbers: Vec<i32> = regex
-        .find_iter(content.as_str())
-        .map(|m| m.as_str().parse::<i32>().unwrap())
-        .collect();
-
-    // Print the numbers
-    let num_of_columns = (numbers.len() as f64).sqrt() as u32;
-    println!("Number of columns: {}", num_of_columns);
-    println!("{:?}", numbers);
+    // Populate the graph
+    for line in content.lines() {        
+        let mut row: Vec<i32> = regex
+            .find_iter(line)
+            .map(|m| m.as_str().parse::<i32>().unwrap())
+            .collect();
+        graph.push(row);
+    }
+    println!("Graph: {:?}", graph);
     
-    
+    //hamiltonian_cycle(&graph);
 }
