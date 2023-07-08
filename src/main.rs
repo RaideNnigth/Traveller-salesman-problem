2c5e0020eee8fef5682ebde1185c6f7f3bda22ad
Gustavo Pinzon Pereira <32282947+RaideNnigth@users.noreply.github.com>
2023-07-08 17:05:44 -0300

    Update main.rs
    Getting parameters from command line and opening file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::process;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // Getting the arguments from the command line
+    // The first argument is the name of the program
+    let args: Vec<_> = env::args().collect();
+    
+    // If no arguments are passed through the command line, exit
+    if args.len() == 1 {
+        println!("No argument send through command line");
+        process::exit(64);
+    }
+    // Get the filepath from the arguments
+    let filepath = &args[1];
+
+    // Print the arguments
+    println!("file path: {}", filepath);   
+    
+    // Open the file, if not possible, exit
+    let mut file = match File::open(filepath) {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Problem opening the file: {:?}", error);
+            process::exit(1);
+        }
+    };
+        
+    let mut content = String::new();
+    file.read_to_string(&mut content)
+        .expect("Should have been able to read the file");
+    println!("{}", content);
+    
 }
