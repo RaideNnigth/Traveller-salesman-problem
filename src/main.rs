d56b9b1b36dafa4f4be45308504328a8df45bf83
Gustavo Pinzon Pereira <32282947+RaideNnigth@users.noreply.github.com>
2023-07-16 16:47:08 -0300

    Finishing aproximative approach
    Finishing 2-aproximative approach for TSP

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-
+use regex::Regex;
+use std::cmp::Ordering;
+use std::cmp::Reverse;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::env;
-use std::process;
-use std::i32::MAX;
-use std::time::SystemTime;
 use std::fs::File;
 use std::io::Read;
-use std::collections::VecDeque;
-use std::cmp::Ordering;
-use regex::Regex;
-use std::cmp::Reverse;
+use std::process;
+use std::time::SystemTime;
 
 #[derive(Clone, Debug, Copy)]
 struct Edge {
@@ -27,7 +26,7 @@ impl PartialEq for Edge {
 
 impl Ord for Edge {
     fn cmp(&self, other: &Self) -> Ordering {
-        return self.weight.cmp(&other.weight)
+        return self.weight.cmp(&other.weight);
     }
 }
 
@@ -41,7 +40,7 @@ fn prim_mst(graph: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     // Adding all the edges to a vector for futher sorting
     let mut edges: Vec<Reverse<Edge>> = Vec::new();
     for i in 0..graph.len() {
-        for j in (i+1)..graph.len() {            
+        for j in (i + 1)..graph.len() {
             if graph[i][j] != 0 {
                 edges.push(Reverse(Edge {
                     src: i as i32,
@@ -53,47 +52,45 @@ fn prim_mst(graph: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     }
     // Sorting it by descending order cause will be used as stack later on
     edges.sort();
-    
+
     // Creating a FIFO queue from the sorted edges
     let mut edges_fifo: VecDeque<Reverse<Edge>> = VecDeque::from(edges.clone());
-    let mut visited: Vec<i32> = Vec::new();
+    let mut visited: HashSet<i32> = HashSet::new();
     let mut mst: Vec<Edge> = Vec::new();
-    
+
     // Adding the frist edge to the MST
     let first_edge = edges_fifo.pop_back().unwrap();
     mst.push(first_edge.0);
-    visited.push(first_edge.0.src);
-    visited.push(first_edge.0.dst);
-    
+    visited.insert(first_edge.0.src);
+    visited.insert(first_edge.0.dst);
+
     // While the MST is not complete
     while mst.len() < graph.len() - 1 {
         let edge = edges_fifo.pop_back().unwrap();
-        if visited.contains(&edge.0.src) && !visited.contains(&edge.0.dst) {
-            visited.push(edge.0.dst);
+        if visited.contains(&edge.0.src) && !visited.contains(&edge.0.dst) ||
+            !visited.contains(&edge.0.src) && visited.contains(&edge.0.dst) {
+            visited.insert(edge.0.dst);
+            visited.insert(edge.0.src);
             mst.push(edge.0);
+            
             let mut temp_vec: Vec<Reverse<Edge>> = edges_fifo.clone().into();
             temp_vec.sort();
             edges_fifo = VecDeque::from(temp_vec);
-        } else if !visited.contains(&edge.0.src) && visited.contains(&edge.0.dst) {
-            visited.push(edge.0.src);
-            mst.push(edge.0);
-            let mut temp_vec: Vec<Reverse<Edge>> = edges_fifo.clone().into();
-            temp_vec.sort();
-            edges_fifo = VecDeque::from(temp_vec);
+            
         } else {
             edges_fifo.push_front(edge);
         }
     }
-    
+
     // Initialize the minimum spanning tree graph with zeros
     let mut mst_graph: Vec<Vec<i32>> = vec![vec![0; graph.len()]; graph.len()];
-        
+
     // Fill in the weights for the minimum spanning tree edges
     for edge in mst {
         mst_graph[edge.src as usize][edge.dst as usize] = edge.weight;
     }
-    
-    return mst_graph
+
+    return mst_graph;
 }
 
 fn mst_to_multigraph(mst: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
@@ -106,21 +103,63 @@ fn mst_to_multigraph(mst: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
             }
         }
     }
-    return multigraph
+    return multigraph;
+}
+
+fn get_all_children_verteces(
+    dad: i32,
+    visited_nodes: &mut Vec<i32>,
+    graph: &Vec<Vec<i32>>,
+) -> VecDeque<i32> {
+    let mut childrens: VecDeque<i32> = VecDeque::new();
+    for i in 0..graph.len() {
+        if graph[dad as usize][i] != 0 && !visited_nodes.contains(&(i as i32)) {
+            childrens.push_back(i as i32);
+        }
+    }
+    return childrens;
+}
+
+fn get_approximative_path(u: i32, multigraph: &Vec<Vec<i32>>, visiteds: &mut Vec<i32>) -> Vec<i32> {
+    let mut childrens: VecDeque<i32> = get_all_children_verteces(u, visiteds, &multigraph);
+    visiteds.push(u);
+    let mut path: Vec<i32> = Vec::new();
+    while !childrens.is_empty() {
+        let current_children: i32 = childrens.pop_back().unwrap();
+        path.append(&mut get_approximative_path(
+            current_children,
+            &multigraph,
+            &mut visiteds.clone(),
+        ));
+    }
+    path.push(u);
+    return path;
 }
 
 fn solve_tsp(graph: &Vec<Vec<i32>>) {
-
     // Start the timer
     let start = SystemTime::now();
-    let mut mst:Vec<Vec<i32>> = prim_mst(graph);
-    let mut multigraph:Vec<Vec<i32>> = mst_to_multigraph(mst);
-        
-    
-    let path: Vec<usize> = Vec::new(); //hamiltonian_cycle(graph);
-    // Stop the timer
-    let total_time = SystemTime::now().duration_since(start).unwrap();    
-    
+    //---------- Get MST and Multigraph of MST -----------
+    let mut mst: Vec<Vec<i32>> = prim_mst(graph);
+    for i in 0..mst.len() {
+        println!("MST: {:?}", mst[i]);
+    }
+    let mut multigraph: Vec<Vec<i32>> = mst_to_multigraph(mst);
+    for i in 0..multigraph.len() {
+        println!("Multigraph: {:?}", multigraph[i]);
+    }
+    //---------- Get Approximative Path ------------------
+    let mut visiteds: Vec<i32> = Vec::new();
+    let path: Vec<i32> = get_approximative_path(0, &multigraph, &mut visiteds);
+    //---------- Inverte path and add source node to it -------------------
+    let mut path: Vec<i32> = path.into_iter().rev().collect();
+    path.push(0);
+    // --------- Stop the timer -------------------
+    let total_time = SystemTime::now().duration_since(start).unwrap();
+    println!("Total time: {}", total_time.as_secs_f64());
+    println!("Path: {:?}", path);
+    println!("Path distance: {}", get_path_distance(graph, &path))
+
     /*
     // If the smallest distance is MAX, there is no solution
 
@@ -134,27 +173,27 @@ fn solve_tsp(graph: &Vec<Vec<i32>>) {
     println!("Solution Exists:");
     println!("Path: {:?}", path);
     println!("Distance: {}", smallest_distance);
-    print!("Time: {}", total_time.as_secs_f64());    
+    print!("Time: {}", total_time.as_secs_f64());
     */
 }
 
-fn get_path_distance(graph: &Vec<Vec<i32>>, path: &Vec<usize>) -> i32 {
+fn get_path_distance(graph: &Vec<Vec<i32>>, path: &Vec<i32>) -> i32 {
     // If the path is empty, return MAX
     let mut distance = 0;
     // Add the distance from the first node to before last node
     for i in 0..path.len() - 1 {
-        distance += graph[path[i]][path[i + 1]];
+        distance += graph[path[i] as usize][path[i + 1] as usize];
     }
     // Add the distance from the last note to the first node to complete the cycle
-    distance += graph[path[path.len() - 1]][path[0]];
-    return distance
+    distance += graph[path[path.len() - 1] as usize][path[0] as usize];
+    return distance;
 }
 
 fn main() {
     // Getting the arguments from the command line
     // The first argument is the name of the program
     let args: Vec<_> = env::args().collect();
-    
+
     // If no arguments are passed through the command line, exit
     if args.len() == 1 {
         println!("No argument send through command line");
@@ -164,8 +203,8 @@ fn main() {
     let filepath = &args[1];
 
     // Print the arguments
-    println!("file path: {}", filepath);   
-    
+    println!("file path: {}", filepath);
+
     // Open the file, if not possible, exit
     let mut file = match File::open(filepath) {
         Ok(file) => file,
@@ -175,25 +214,24 @@ fn main() {
         }
     };
 
-    // Read the file into a string        
+    // Read the file into a string
     let mut content = String::new();
     file.read_to_string(&mut content)
-        .expect("Should have been able to read the file");    
-    
+        .expect("Should have been able to read the file");
+
     // Create a vector of vectors to store the graph
     let mut graph: Vec<Vec<i32>> = Vec::new();
     // Create a regex to find all the numbers in the file
     let regex = Regex::new(r"\d+").unwrap();
     // Populate the graph
-    for line in content.lines() {        
+    for line in content.lines() {
         let row: Vec<i32> = regex
             .find_iter(line)
             .map(|m| m.as_str().parse::<i32>().unwrap())
             .collect();
         graph.push(row);
     }
-    
+
     // Call the solve_tsp function
     solve_tsp(&graph);
-    
 }
